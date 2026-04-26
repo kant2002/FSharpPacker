4cb37ff99003afbc0530aec5733a23077fdcbc8d
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-26 22:30:01 +0200

    Update NixOS flake package (#26)
    * Update Nixos build instructions

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Tool for packaging FSX files as self-contained executables";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
